157d490468b4efcb84d753d714fb561915ee0225
BlizniukT <blizniuk1.20@gmail.com>
2021-02-02 10:18:59 +0300

    Add reader and writer of users from json file

--- src/main/java/by/food/orders/logics/AuthenticationManager.java
+++ src/main/java/by/food/orders/logics/AuthenticationManager.java
@@ -2,6 +2,8 @@ package by.food.orders.logics;
 
 import by.food.orders.data.dao.api.UserDao;
 import by.food.orders.entity.User;
+import by.food.orders.exception.DaoException;
+import by.food.orders.exception.DataException;
 import by.food.orders.exception.NoSuchUserException;
 
 import java.util.Optional;
@@ -13,9 +15,8 @@ public class AuthenticationManager {
         this.userDao = userDao;
     }
 
-    public User authenticateByCredentials(String login, String pass) throws NoSuchUserException {
+    public User authenticateByCredentials(String login, String pass) throws NoSuchUserException, DaoException {
         Optional<User> optionalUser = userDao.findByCredentials(login, pass);
-        User user = optionalUser.orElseThrow(() -> new NoSuchUserException("user is not found!"));
-        return user;
+        return optionalUser.orElseThrow(() -> new NoSuchUserException("user is not found!"));
     }
 }
